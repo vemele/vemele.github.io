3a1b6b8cc8f44d5b4c2ab1d8ba03308f6fc9b6d3
veronica mery <veronicamery@veronicas-MacBook-Pro.local>
2025-04-09 17:21:53 -0400

    Actualizar sitio con imagen y diseño minimalista

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
   });
 
   eleventyConfig.addPassthroughCopy("styles");
-
+  eleventyConfig.addPassthroughCopy("assets");
   return {
     dir: {
       input: "src",
