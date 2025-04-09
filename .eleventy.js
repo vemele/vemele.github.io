4ff4cdb40a9c05926ed72cdd83d53f0b48b226e4
veronica mery <veronicamery@veronicas-MacBook-Pro.local>
2025-04-09 16:31:39 -0400

    Generar sitio estático para GitHub Pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,17 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  // Filtro de fecha personalizado
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
     return DateTime.fromJSDate(new Date(value)).toFormat(format);
   });
 
-  // Copia los estilos sin tocarlos
   eleventyConfig.addPassthroughCopy("styles");
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "dist"
+      output: "docs" // 👈 ¡Esto es lo importante!
     }
   };
 };
