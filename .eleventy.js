0b60863fb7871a4fd86351db1abdfe7ba87722f3
veronica mery <veronicamery@veronicas-MacBook-Pro.local>
2025-04-10 15:57:58 -0400

    Ajustes en la estructura y configuración para la sección de publicaciones

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+  // Filtro para formatear fechas
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
     return DateTime.fromJSDate(new Date(value)).toFormat(format);
   });
 
+  // Colección de posts
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./src/posts/*.md").reverse();
   });
 
+  // Archivos estáticos
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("assets");
 
--- src/posts/mi-primer-post.md
+++ src/posts/mi-primer-post.md
@@ -1,7 +1,7 @@
 ---
 layout: layout.njk
 title: Mi primer post
-date: 2024-04-11
+date: 2025-04-10
 ---
 
 Este es el contenido de mi primer post ✨  
