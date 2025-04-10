0a1176352e09b2a7ca57116d39f08ade02608483
veronica mery <veronicamery@veronicas-MacBook-Pro.local>
2025-04-10 15:16:03 -0400

    Agregar colección de posts para listado automático

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,25 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+  // Filtro para formatear fechas
   eleventyConfig.addFilter("date", (value, format = "yyyy") => {
     return DateTime.fromJSDate(new Date(value)).toFormat(format);
   });
 
+  // Colección de posts
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/posts/*.md").reverse();
+  });
+
+  // Archivos estáticos
   eleventyConfig.addPassthroughCopy("styles");
   eleventyConfig.addPassthroughCopy("assets");
+
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "docs" // 👈 ¡Esto es lo importante!
+      output: "docs"
     }
   };
 };
